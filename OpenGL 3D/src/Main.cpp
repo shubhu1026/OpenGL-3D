c735b65f551dd3a48423685a8e22267eb2de72d8
shubhu1026 <shubhu.p2000@gmail.com>
2023-07-11 20:37:29 +0530

    Framebuffers and post processing

--- OpenGL 3D/src/Main.cpp
+++ OpenGL 3D/src/Main.cpp
@@ -3,21 +3,17 @@
 const unsigned int width = 800;
 const unsigned int height = 800;
 
-// Takes care of the information needed to draw the windows
-const unsigned int numWindows = 100;
-glm::vec3 positionsWin[numWindows];
-float rotationsWin[numWindows];
-
-// Takes care of drawing the windows in the right order
-unsigned int orderDraw[numWindows];
-float distanceCamera[numWindows];
-
-// Compare function
-int compare(const void* a, const void* b)
+float rectangleVertices[] =
 {
-	double diff = distanceCamera[*(int*)b] - distanceCamera[*(int*)a];
-	return  (0 < diff) - (diff < 0);
-}
+	// Coords    // texCoords
+	 1.0f, -1.0f,  1.0f, 0.0f,
+	-1.0f, -1.0f,  0.0f, 0.0f,
+	-1.0f,  1.0f,  0.0f, 1.0f,
+
+	 1.0f,  1.0f,  1.0f, 1.0f,
+	 1.0f, -1.0f,  1.0f, 0.0f,
+	-1.0f,  1.0f,  0.0f, 1.0f
+};
 
 int main()
 {
@@ -52,8 +48,7 @@ int main()
 
 	// Generates Shader object using shaders default.vert and default.frag
 	Shader shaderProgram("res/shaders/default.vert", "res/shaders/default.frag");
-	Shader grassProgram("res/shaders/default.vert", "res/shaders/grass.frag");
-	Shader winProgram("res/shaders/default.vert", "res/shaders/windows.frag");
+	Shader framebufferProgram("res/shaders/framebuffer.vert", "res/shaders/framebuffer.frag");
 
 	// Take care of all the light related things
 	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
@@ -65,9 +60,8 @@ int main()
 	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
 	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
 
-	grassProgram.Activate();
-	glUniform4f(glGetUniformLocation(grassProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
-	glUniform3f(glGetUniformLocation(grassProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+	framebufferProgram.Activate();
+	glUniform1i(glGetUniformLocation(framebufferProgram.ID, "screenTexture"), 0);
 
 	// Enables the Depth Buffer
 	glEnable(GL_DEPTH_TEST);
@@ -86,9 +80,18 @@ int main()
 	// Creates camera object
 	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
-	Model grass("res/models/grass/scene.gltf");
-	Model ground("res/models/ground2/scene.gltf");
-	Model windows("res/models/windows/scene.gltf");
+	Model model("res/models/crow/scene.gltf");
+
+	unsigned int rectVAO, rectVBO;
+	glGenVertexArrays(1, &rectVAO);
+	glGenBuffers(1, &rectVBO);
+	glBindVertexArray(rectVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, rectVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(rectangleVertices), &rectangleVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 
 	// Fps Counter variables
 	double prevTime = 0.0;
@@ -96,18 +99,30 @@ int main()
 	double timeDiff;
 	unsigned int counter = 0;
 
-	//glfwSwapInterval(0);
+	unsigned int FBO;
+	glGenFramebuffers(1, &FBO);
+	glBindFramebuffer(GL_FRAMEBUFFER, FBO);
 
-	for (unsigned int i = 0; i < numWindows; i++)
+	unsigned int framebufferTexture;
+	glGenTextures(1, &framebufferTexture);
+	glBindTexture(GL_TEXTURE_2D, framebufferTexture);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferTexture, 0);
+
+	unsigned int RBO;
+	glGenRenderbuffers(1, &RBO);
+	glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+	glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
+
+	auto fboStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	if (fboStatus != GL_FRAMEBUFFER_COMPLETE)
 	{
-		positionsWin[i] = glm::vec3
-		(
-			-15.0f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (15.0f - (-15.0f)))),
-			1.0f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (4.0f - 1.0f))),
-			-15.0f + static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (15.0f - (-15.0f))))
-		);
-		rotationsWin[i] = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / 1.0f));
-		orderDraw[i] = i;
+		std::cout << "Framebuffer error: " << fboStatus << std::endl;
 	}
 
 	// Main while loop
@@ -128,37 +143,27 @@ int main()
 			camera.Inputs(window);
 		}
 
+		glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// Clean the back buffer and depth buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glEnable(GL_DEPTH_TEST);
 
 		// Handles camera inputs
 		// Updates and exports the camera matrix to the Vertex Shader
 		camera.UpdateMatrix(45.0f, 0.1f, 100.0f);
 
 		// Draw the normal model
-		ground.Draw(shaderProgram, camera);
+		model.Draw(shaderProgram, camera);
 
-		// Disable cull face so that grass and windows have both faces
-		glDisable(GL_CULL_FACE);
-		grass.Draw(grassProgram, camera);
-		// Enable blending for windows
-		glEnable(GL_BLEND);
-		// Get distance from each window to the camera
-		for (unsigned int i = 0; i < numWindows; i++)
-		{
-			distanceCamera[i] = glm::length(camera.Position - positionsWin[i]);
-		}
-		// Sort windows by distance from camera
-		qsort(orderDraw, numWindows, sizeof(unsigned int), compare);
-		// Draw windows
-		for (unsigned int i = 0; i < numWindows; i++)
-		{
-			windows.Draw(winProgram, camera, positionsWin[orderDraw[i]], glm::quat(1.0f, 0.0f, rotationsWin[orderDraw[i]], 0.0f));
-		}
-		glDisable(GL_BLEND);
-		glEnable(GL_CULL_FACE);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		framebufferProgram.Activate();
+		glBindVertexArray(rectVAO);
+		glDisable(GL_DEPTH_TEST);
+		glBindTexture(GL_TEXTURE_2D, framebufferTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
@@ -170,7 +175,7 @@ int main()
 
 	// Delete all the objects we've created
 	shaderProgram.Delete();
-	grassProgram.Delete();
+	framebufferProgram.Delete();
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
 	// Terminate GLFW before ending the program
